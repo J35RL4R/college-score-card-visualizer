61fa374a11d4b9bd3a5aa47103245937e0bc2448
Claire Vita <clairej.vita@gmail.com>
2020-12-04 19:47:13 -0500

    full functioning api call / login

--- public/js/search.js
+++ public/js/search.js
@@ -1,18 +1,19 @@
 $(function () {
 
-    $("#search-input").click((event) => {
+    $("#search-submit").click((event) => {
         event.preventDefault();
-        let schoolName = $("#input").val();
-        findSchool(schoolName);
-        $('div').removeClass('hide');
-  
-  
-      })
+        let schoolName = $("#search-input").val();
+        schoolArr = schoolName.split(" ");
+        schoolStr = schoolArr.join("%20");
+        console.log(schoolStr);
+        findSchool(schoolStr);
+      });
+
     function findSchool(schoolName) {
+    
+      var cscAPI = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=" + schoolName + "&fields=latest&api_key=hDvhnFr1dwbR1ItiqY6TL9Epf3Isbcd1QHHZq9Sb";
   
-      var cscAPI = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=" + schoolName; "&fields=latest&api_key=hDvhnFr1dwbR1ItiqY6TL9Epf3Isbcd1QHHZq9Sb";
-  
-      var pokeGif = "https://api.giphy.com/v1/gifs/search?api_key=I4lc5z5Pjva79mvZq3suogFUfDYu5RbM&q=" + schoolName; "&limit=1&offset=0&rating=g&lang=en";
+      //var pokeGif = "https://api.giphy.com/v1/gifs/search?api_key=I4lc5z5Pjva79mvZq3suogFUfDYu5RbM&q=" + schoolName; "&limit=1&offset=0&rating=g&lang=en";
   
       console.log(cscAPI);
       console.log(schoolName);
